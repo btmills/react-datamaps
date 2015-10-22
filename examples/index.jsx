c1c6bdea0bd5ca39f89248942ff5b514c1641cf6
Brandon Mills <mills.brandont@gmail.com>
2015-10-22 13:11:20 -0400

    Add options to examples

--- examples/index.jsx
+++ examples/index.jsx
@@ -3,7 +3,45 @@ import ReactDOM from 'react-dom';
 
 import Datamap from '../src';
 
+const App = React.createClass({
+
+	displayName: 'App',
+
+	getInitialState() {
+		return {
+			height: 300,
+			scope: 'world',
+			width: 500
+		};
+	},
+
+	update() {
+		this.setState({
+			height: parseInt(this.refs.height.value, 10) || null,
+			scope: this.refs.scope.value || null,
+			width: parseInt(this.refs.width.value, 10) || null
+		});
+	},
+
+	render() {
+		return (
+			<div className="App">
+				<div className="App-options">
+					<label>Height <input ref="height" type="number" /></label>
+					<label>Scope <input ref="scope" type="text" /></label>
+					<label>Width <input ref="width" type="number" /></label>
+					<button onClick={this.update}>Update</button>
+				</div>
+				<div className="App-map">
+					<Datamap {...this.state} />
+				</div>
+			</div>
+		);
+	}
+
+});
+
 ReactDOM.render(
-	<Datamap />,
+	<App />,
 	document.getElementById('app')
 );
