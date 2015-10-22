9a5f000e96fbe516cfa7acc1a4b52660a5e257bd
Brandon Mills <mills.brandont@gmail.com>
2015-10-22 11:52:31 -0400

    Add example

---
+++ examples/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Datamap from '../src';
+
+ReactDOM.render(
+	<Datamap />,
+	document.getElementById('app')
+);
---
+++ examples/webpack.config.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	devtool: 'eval',
+
+	entry: './index.jsx',
+
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: './bundle.js'
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html',
+			inject: 'body',
+			filename: 'index.html'
+		}),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('development')
+		})
+	],
+
+	module: {
+		loaders: [
+			{
+				test: /\.json$/,
+				loader: 'json'
+			},
+			{
+				test: /\.jsx?$/,
+				include: [
+					__dirname,
+					path.resolve(__dirname, '../src')
+				],
+				loader: 'babel?stage=0'
+			}
+		]
+	},
+	resolve: {
+		root: [
+			path.resolve(__dirname, 'node_modules'),
+			path.resolve(__dirname, '../node_modules')
+		],
+		extensions: ['', '.js', '.jsx']
+	},
+	resolveLoader: {
+		root: path.resolve(__dirname, 'node_modules')
+	}
+};
