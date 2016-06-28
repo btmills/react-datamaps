9e14ac7c5194e915b1dd103bda7558847231c55f
Brandon Mills <mills.brandont@gmail.com>
2016-06-27 23:39:09 -0400

    Convert Datamap to ES6 class

--- src/datamap.jsx
+++ src/datamap.jsx
@@ -1,47 +1,46 @@
 import React from 'react';
-import Datamap from 'datamaps';
+import Datamaps from 'datamaps';
 
-export default React.createClass({
+export default class Datamap extends React.Component {
 
-	displayName: 'Datamap',
-
-	propTypes: {
+	static propTypes = {
 		arc: React.PropTypes.array,
 		arcOptions: React.PropTypes.object,
 		bubbleOptions: React.PropTypes.object,
 		bubbles: React.PropTypes.array,
 		graticule: React.PropTypes.bool,
 		labels: React.PropTypes.bool
-	},
+	};
 
 	componentDidMount() {
 		this.drawMap();
-	},
+	}
 
 	componentWillReceiveProps() {
 		this.clear();
-	},
+	}
 
 	componentDidUpdate() {
 		this.drawMap();
-	},
+	}
 
 	componentWillUnmount() {
 		this.clear();
-	},
+	}
 
 	clear() {
-		const container = this.refs.container;
+		const { container } = this.refs;
 
 		for (const child of Array.from(container.childNodes)) {
 			container.removeChild(child);
 		}
-	},
+	}
 
 	drawMap() {
-		const map = new Datamap(Object.assign({}, { ...this.props }, {
+		const map = new Datamaps({
+			...this.props,
 			element: this.refs.container
-		}));
+		});
 
 		if (this.props.arc) {
 			map.arc(this.props.arc, this.props.arcOptions);
@@ -58,14 +57,14 @@ export default React.createClass({
 		if (this.props.labels) {
 			map.labels();
 		}
-	},
+	}
 
 	render() {
 		const style = {
 			position: 'relative'
 		};
 
-		return <div ref="container" style={style}></div>;
+		return <div ref="container" style={style} />;
 	}
 
-});
+}
